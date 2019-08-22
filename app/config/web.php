2d826fa8d5af97825d81a445d9c8ef239fae1d22
skazkin <russkazkin@gmail.com>
2019-08-22 07:37:58 +0300

    add auth molule
    gii generated module auth added

--- app/config/web.php
+++ app/config/web.php
@@ -12,6 +12,11 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'auth' => [
+            'class' => 'app\modules\auth\Module'
+        ]
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
