5221018467c76713f70a7d21f2935567a17c2102
skazkin <russkazkin@gmail.com>
2019-08-30 14:05:25 +0300

    add password reset action for auth modele
    auth module is functional

--- app/modules/auth/controllers/AuthController.php
+++ app/modules/auth/controllers/AuthController.php
@@ -7,6 +7,7 @@ namespace app\modules\auth\controllers;
 use app\modules\auth\controllers\actions\AuthRequestPasswordResetAction;
 use app\modules\auth\controllers\actions\AuthSignInAction;
 use app\modules\auth\controllers\actions\AuthSignUpAction;
+use app\modules\auth\controllers\actions\ResetPasswordAction;
 use app\modules\auth\controllers\base\BaseController;
 
 class AuthController extends BaseController
@@ -23,7 +24,8 @@ class AuthController extends BaseController
             ],
             'sign-up' => ['class' => AuthSignUpAction::class],
             'sign-in' => ['class' => AuthSignInAction::class],
-            'request-password-reset' => ['class' => AuthRequestPasswordResetAction::class]
+            'request-password-reset' => ['class' => AuthRequestPasswordResetAction::class],
+            'password-reset' => ['class' => ResetPasswordAction::class],
         ];
     }
 }
